16e19e9f43e0ade3b7c76082067ce56ba856eae9
Joelsv95 <157936456+Joelsv95@users.noreply.github.com>
2024-03-27 11:03:11 +0000

    First Commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const getRandomEmlement = (array) => {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+  }
+  
+  const getGuilty = () => {
+    const who = ['the dog', 'my grandma', 'his turtle', 'my bird']
+    return getRandomElement(who);
+  }
+  const getAction = () => {
+    const action = ['ate', 'peed', 'crushed', 'broke'];
+   return getRandomElement(action);
+  }
+  const getTarget = () =>{
+    let what = ['my homework', 'my phone', 'the car'];
+    return getRandomElement(what);
+  }
+  const getWhen = () => {
+    const when = ['before the class', 'right on time', 'when I finished', 'during my lunch'];
+   return getRandomElement(when);
+  }
+  const getMessage = (guilty, action, target, when) => {
+    return `${guilty} ${action} ${target} ${when}`;
+  }
+  const printMessage = (message) => {
+    console.log(message);
+  }
+  
+  function main (); {
+  const guilty = getGuilty();
+  const action = getAction();
+  const target = getTarget();
+  const when = getWhen();
+  
+    const message = getMessage(guilty,action,target,when);
+    printMessage(message);
+ }
+  
+main();
